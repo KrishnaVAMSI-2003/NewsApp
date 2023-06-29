fc565dab9e671067f7faadc40ac3cecae0495b96
Krishna Vamsi <krishnavamsi052003@gmail.com>
2023-06-29 11:00:36 +0530

    handled api fetching problem

--- src/newsProject/NewsApp.js
+++ src/newsProject/NewsApp.js
@@ -3,27 +3,39 @@ import RenderNews from "./RenderNews"
 import Pagination from "./Pagination"
 import Header from "./Header"
 import './news.css'
+import News from './news.json'
 
 export default function NewsApp() {
     const [search, setSearch] = useState("")
     const [news, setNews] = useState()
     const [input, setInput] = useState()
     const [pageNo, setPageNo] = useState(1)
+    const [api, setApi] = useState(false)
+    const [submit, setSubmit] = useState(false)
     
 
-    useEffect( ()=>{
+    useEffect(()=>{
         const fetchNews = async() => {
+            const date = new Date()
+            const searchdate = `${date.getFullYear()}-${date.getMonth()}-${date.getDate()}`
             const data = await fetch(`
-            https://newsapi.org/v2/everything?q=${search}&from=2023-06-10&sortBy=publishedAt&apiKey=8d569128218f497a9d7bb8cbf8f33177&pageSize=9&page=${pageNo}`)
+            https://newsapi.org/v2/everything?q=${search}&from=${searchdate}&sortBy=publishedAt&apiKey=ffaae82da15b4bbc89190dbb311a970b&pageSize=9&page=${pageNo}`)
             const jsonData = await data.json()
-            setNews(jsonData.articles)
+            if(jsonData.articles){
+                setApi(true)
+                setNews(jsonData.articles)
+            }
+            else
+                setNews(News.articles.filter((ele,index)=>index>=pageNo && index<pageNo+9))
         }
         fetchNews()
     },[search,pageNo])
 
     const handleSubmit = (e) => {
         e.preventDefault()
+        setPageNo(1)
         setSearch(input)
+        setSubmit(true)
     }
 
     return(
@@ -33,8 +45,9 @@ export default function NewsApp() {
                 <input type='text' value={input} placeholder="search news" onChange={(e)=>{setInput(e.target.value)}}/>
                 <button type='submit'>Search news</button>
             </form>
-            {news ?
-            <div className="render"> 
+            {submit ?
+            <div className="render">
+                {!api&&<h2>Unable to render news from api due to some issues, displaying sample news</h2>}
                 <div className="container">
                     {news && news.map((ele)=>{
                         return <RenderNews urlToImage={ele.urlToImage} title={ele.title} description={ele.description} />
